dcde047cdda49c9cbcc328e90762bd4c7dd5e41d
Farai Major <faraimutukumira@gmail.com>
2023-03-11 23:49:53 -0500

    render all images

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -6,11 +6,6 @@ export default function Meme() {
     const [meme, setMeme] = React.useState(memesData.data.memes)
     const [allMemes, setAllMemes] = React.useState([]);
 
-    const src_link = meme.map(link => {
-        return (
-            link.url
-        )
-    })
     return (
         <main>
             <form className="form">
@@ -30,11 +25,13 @@ export default function Meme() {
                     Get a new meme image 🖼
                 </button>
             </form>
-            <img style={{ marginLeft: "450px" }} src={meme.map(link => {
-                return (
-                    link.url
-                )
-            })} />
+            <div className='images'>
+                {meme.map(link => {
+                    return (
+                        <img src={link.url} />
+                    )
+                })}
+            </div>
         </main>
     )
 }
